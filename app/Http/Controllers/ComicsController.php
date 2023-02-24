4f3da5478a63bde04ca678e2afd5f1a9a8de965d
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-02-24 09:57:36 +0100

    controller fix

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -14,6 +14,7 @@ class ComicsController extends Controller
      */
     public function index()
     {
+        $comics = Comic::all();
         $pages = config('comics.pages');
         return view('comics', compact('comics', 'pages'));
     }
