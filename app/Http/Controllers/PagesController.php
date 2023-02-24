1aabf92b7bd0491986ed336d09f6a92951d4b304
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-02-24 09:36:20 +0100

    controllers created

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,11 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Comic as Comic;
 
-class ComicController extends Controller
+class PagesController extends Controller
 {
     public function index() {
-        $comics = Comic::all();
         $pages = config('comics.pages');
-        return view('index', compact('comics', 'pages'));
+        return view('comics', compact('pages'));
     }
 }
