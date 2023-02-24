ac258f0442425c7bd447629518c583dcf85bf983
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-02-24 09:38:07 +0100

    title and header links updated

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,12 +7,12 @@
 	</div>
 	<div class="container px-0">
 		<div class="row">
-			<div class="col py-3"><img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt=""></div>
+			<div class="col py-3"><a href="/"><img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt=""></a></div>
 			<div class="col">
 				<nav class="h-100">
 					<ul class="d-flex align-items-center mb-0 h-100 fw-bold p-0 gap-4">
 						@foreach ($pages as $item)
-							<li class="h-100 d-flex align-items-center nav-voice"><a href="#">{{ $item['voice'] }}</a></li>
+							<li class="h-100 d-flex align-items-center nav-voice"><a href="{{ $item['url'] }}">{{ $item['voice'] }}</a></li>
 						@endforeach
 					</ul>
 				</nav>
